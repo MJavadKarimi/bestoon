3a600a031819c7cfe8a0c8959934e14bd127fe7a
unknown <MJavadKarimi>
2023-02-19 17:27:05 +0330

    submit/expense and submit/income web services

--- web/models.py
+++ web/models.py
@@ -3,6 +3,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+
+    def __str__(self):
+        return f"{self.user} - token"
+
 class Expense(models.Model):
     title = models.CharField(max_length=255)
     amount = models.BigIntegerField()
--- web/views.py
+++ web/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from json import JSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+from .models import User, Token, Expense, Income
+from datetime import datetime
 
 # Create your views here.
+
+@csrf_exempt
+
+def submit_income(request):
+    """user submit an income"""
+
+    #TODO: validate data. user might be fake. token might be fake. amount might be...
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token=this_token).get()
+    if 'date' not in request.POST:
+        date = datetime.now()
+    Income.objects.create(title=request.POST['title'], amount=request.POST['amount'], date=date, user=this_user)
+
+    return JsonResponse({
+        'status': 'ok'
+    }, encoder=JSONEncoder)
+
+
+
+def submit_expense(request):
+    """user submit an expense"""
+
+    #TODO: validate data. user might be fake. token might be fake. amount might be...
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token=this_token).get()
+    if 'date' not in request.POST:
+        date = datetime.now()
+    Expense.objects.create(title=request.POST['title'], amount=request.POST['amount'], date=date, user=this_user)
+
+    return JsonResponse({
+        'status': 'ok'
+    }, encoder=JSONEncoder)
